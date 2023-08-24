a891169840ab47becf43639313e1be068374226b
Mat-Algo <matthewmodisseril@gmail.com>
2023-08-24 19:33:37 +0530

    model evaluation completed

---
+++ src/textSummarizer/pipeline/stage_05_model_evaluation.py
@@ -0,0 +1,17 @@
+from src.textSummarizer.config.configuration import ConfigurationManager
+from src.textSummarizer.components.model_evaluation import ModelEvaluation
+from src.textSummarizer.logging import logger
+
+class ModelTrainerTrainingPipeline:
+    def __init__(self):
+        pass
+    def main(self):
+        try:
+            config = ConfigurationManager()
+            model_evaluation_config = config.get_model_evaluation_config()
+            model_evaluation_config = ModelEvaluation(config=model_evaluation_config)
+            model_evaluation_config.evaluate()
+        except Exception as e:
+            raise e
+
+
