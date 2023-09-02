d0ed5df927c4b9968626e4d2948884c9e4fb5753
Mat-Algo <matthewmodisseril@gmail.com>
2023-09-03 00:27:06 +0530

    port added

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ async def predict_route(text):
         raise e
 
 
-# if __name__ == "__main__":
-#     uvicorn.run(app, host = "0.0.0.0", port = 8080)
+if __name__ == "__main__":
+    uvicorn.run(app, host = "0.0.0.0", port = 8080)
     
 
 
